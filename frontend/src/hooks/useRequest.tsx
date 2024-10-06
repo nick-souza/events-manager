da76ef1ca7349fb3272e8f3a9112436d6d29c1a9
Nicolas Sa de Souza <nsds26@hotmail.com>
2024-10-06 11:52:15 -0300

    feat: add api service v1 and userRequest v1

---
+++ frontend/src/hooks/useRequest.tsx
@@ -0,0 +1,61 @@
+import { AxiosError, AxiosRequestConfig } from "axios";
+import { useState } from "react";
+import { api } from "../services/api";
+
+interface UseRequestResult {
+	loading: boolean;
+	request: {
+		get: <T>(url: string, config?: AxiosRequestConfig) => Promise<Response<T>>;
+		post: <T>(url: string, data: T, config?: AxiosRequestConfig) => Promise<Response<T>>;
+		put: <T>(url: string, data: T, config?: AxiosRequestConfig) => Promise<Response<T>>;
+		patch: <T>(url: string, data: T, config?: AxiosRequestConfig) => Promise<Response<T>>;
+		delete: <T>(url: string, config?: AxiosRequestConfig) => Promise<Response<T>>;
+	};
+}
+
+type Response<T> = {
+	data: T | null;
+	success: boolean;
+};
+
+type ErrorObj = {
+	message: string;
+};
+
+export const useRequest = (): UseRequestResult => {
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const handleRequest = async <T,>(request: () => Promise<T>): Promise<Response<T>> => {
+		setLoading(true);
+
+		try {
+			const result = await request();
+
+			return { data: result, success: true } as Response<T>;
+		} catch (err) {
+			errorHandler(err as AxiosError<ErrorObj>);
+		} finally {
+			setLoading(false);
+		}
+
+		return { data: null, success: false } as Response<T>;
+	};
+
+	const errorHandler = (errorResponse: AxiosError<ErrorObj>) => {
+		const message = errorResponse.response?.data?.message;
+
+		console.log("Error: ", message);
+
+		// TODO: Implement error handling
+	};
+
+	const request = {
+		get: <T,>(url: string, config?: AxiosRequestConfig) => handleRequest(() => api.get<T>(url, config)),
+		post: <T,>(url: string, data: T, config?: AxiosRequestConfig) => handleRequest(() => api.post<T>(url, data, config)),
+		put: <T,>(url: string, data: T, config?: AxiosRequestConfig) => handleRequest(() => api.put<T>(url, data, config)),
+		patch: <T,>(url: string, data: T, config?: AxiosRequestConfig) => handleRequest(() => api.patch<T>(url, data, config)),
+		delete: <T,>(url: string, config?: AxiosRequestConfig) => handleRequest(() => api.delete<T>(url, config)),
+	};
+
+	return { loading, request };
+};
