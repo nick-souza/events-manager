4d1231ce96b7e60294f566b1e077bd297b565578
Nicolas Sa de Souza <nsds26@hotmail.com>
2024-10-07 10:35:32 -0300

    refactor: removed unused methods

--- backend/src/main/java/com/nicolas/EventManager/repositories/EventRepository.java
+++ backend/src/main/java/com/nicolas/EventManager/repositories/EventRepository.java
@@ -1,10 +1,8 @@
 package com.nicolas.EventManager.repositories;
 
 import com.nicolas.EventManager.domain.Event;
-import com.nicolas.EventManager.domain.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,11 +10,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     Optional<Event> findEventById(long id);
 
-    List<Event> findAllByStatus(EventStatus status);
-
     List<Event> findAllByOrderByStartDateAsc();
-
-    List<Event> findByStartDate(LocalDateTime startDate);
-
-    List<Event> findByStartDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
